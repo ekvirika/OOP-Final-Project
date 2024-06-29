7a340213390caa9f9628407f692c27d03ee63f66
nkikn <nkikn21@freeuni.edu.ge>
2024-06-29 15:02:59 +0400

    ADD: Impelement PasswordHasher.java

--- OOP-Final-Project/src/main/java/Models/PasswordHasher.java
+++ OOP-Final-Project/src/main/java/Models/PasswordHasher.java
@@ -1,4 +1,29 @@
 package Models;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class PasswordHasher {
+
+    public static  String generateSalt(){
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+
+    private static  String hash(String password, String salt) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(salt.getBytes());
+        byte[] hashed = md.digest(password.getBytes());
+        return Base64.getEncoder().encodeToString(hashed);
+    }
+
+    public  String hashedPassword(String password) throws NoSuchAlgorithmException {
+        String salt = generateSalt();
+        String hashed = hash(password,salt);
+        return salt + " : " + hashed;
+    }
 }
