b9595437eb257f45207dfb00be096388e1b97aa3
nkikn <nkikn21@freeuni.edu.ge>
2024-07-05 18:40:06 +0400

    UPDATE: SQL_all.sql and QuestionType.java

--- OOP-Final-Project/src/main/java/Models/Enums/QuestionType.java
+++ OOP-Final-Project/src/main/java/Models/Enums/QuestionType.java
@@ -4,8 +4,8 @@ package Models.Enums;
  * Enum representing the various types of questions.
  */
 public enum QuestionType {
-    FILL_IN_THE_BLANK,        /**< A question with a blank that needs to be filled in. */
     QUESTION_RESPONSE,        /**< A question that requires a textual response. */
+    FILL_IN_THE_BLANK,        /**< A question with a blank that needs to be filled in. */
     MULTIPLE_CHOICE,          /**< A question with multiple choice answers. */
     PICTURE_RESPONSE,         /**< A question that requires a response based on a picture. */
     MULTI_ANSWER,             /**< A question with multiple fields to answer. */
