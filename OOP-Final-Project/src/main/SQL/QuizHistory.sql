30b12f955173f62586c62b48213c2724d0d59ee1
ekvirika <ellekvirikashvili@gmail.com>
2024-07-05 18:22:15 +0400

    CHANGE: structure change in QuizHistory database to allow each user multiple quiz attempts

--- OOP-Final-Project/src/main/SQL/QuizHistory.sql
+++ OOP-Final-Project/src/main/SQL/QuizHistory.sql
@@ -1,11 +1,11 @@
 CREATE TABLE QuizHistory (
+                             id INT AUTO_INCREMENT PRIMARY KEY,
                              quizId INT NOT NULL,
                              username VARCHAR(255) NOT NULL,
                              quizScore INT DEFAULT 0,
                              startTime TIME,
                              endTime TIME,
                              elapsedTime BIGINT DEFAULT 0,
-                             PRIMARY KEY (quizId, username),
                              FOREIGN KEY (quizId) REFERENCES Quiz(quizId),
-                             FOREIGN KEY (username) REFERENCES User(username)
+                             FOREIGN KEY (username) REFERENCES Account(username)
 );
