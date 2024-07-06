a7496783a3291b2b5b7a38633c82081e476d2d57
ttsut21 <ttsut21@freeuni.edu.ge>
2024-07-06 22:03:28 +0400

    ADD: Achievements script

--- OOP-Final-Project/src/main/SQL/SQL_all.sql
+++ OOP-Final-Project/src/main/SQL/SQL_all.sql
@@ -108,6 +108,28 @@ CREATE TABLE IF NOT EXISTS Notifications (
 
 
 # ----------------------------------------------------------------------------
+
+-- Insert achievements
+INSERT INTO Achievement (achievementId, achievementName, achievementUrl, achievementDescription)
+VALUES (1, 'Amateur Author', 'https://img.icons8.com/?size=100&id=PDVbwX9qe2CX&format=png&color=000000', 'The user created a quiz');
+
+INSERT INTO Achievement (achievementId, achievementName, achievementUrl, achievementDescription)
+VALUES (2, 'Prolific Author', 'https://img.icons8.com/?size=100&id=68706&format=png&color=000000', 'The user created five quizzes');
+
+INSERT INTO Achievement (achievementId, achievementName, achievementUrl, achievementDescription)
+VALUES (3, 'Prodigious Author', 'https://img.icons8.com/?size=100&id=yrPIS24okymU&format=png&color=000000', 'The user created ten quizzes');
+
+INSERT INTO Achievement (achievementId, achievementName, achievementUrl, achievementDescription)
+VALUES (4, 'Quiz Machine', 'https://img.icons8.com/?size=100&id=114898&format=png&color=000000', 'The user took ten quizzes');
+
+INSERT INTO Achievement (achievementId, achievementName, achievementUrl, achievementDescription)
+VALUES (5, 'I am the Greatest', 'https://img.icons8.com/?size=100&id=80747&format=png&color=000000', 'The user had the highest score on a quiz. Note, once earned this
+achievement does not go away if someone else later bests the high score');
+
+INSERT INTO Achievement (achievementId, achievementName, achievementUrl, achievementDescription)
+VALUES (6, 'Practice Makes Perfect', 'https://img.icons8.com/?size=100&id=114206&format=png&color=000000', 'The user took a quiz in practice mode.');
+
+
 -- Insert a quiz
 INSERT INTO Quiz (username, quizName, quizDescription, quizScore, questionIds, isSinglePage, randomizeQuestions,
                   immediateFeedback, createTime)
